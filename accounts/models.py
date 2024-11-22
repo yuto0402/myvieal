57b7b849855b44156c66167682872b08ab935c6d
monchan <simon.torii@icloud.com>
2024-11-22 21:57:06 +0900

    Delete FollowingModel and Add following field in CustomUserModel

--- accounts/models.py
+++ accounts/models.py
@@ -5,3 +5,4 @@ from django.db import models
 class CustomUser(AbstractUser):
     icon_image = models.ImageField(upload_to="user/", null=True, blank=False)
     introduction = models.TextField(blank=True)
+    following = models.ManyToManyField("self", related_name="followed_by", symmetrical=False, blank=True)
