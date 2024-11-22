c0ce7970ebbd04c97a2cce2121b0593273cb4256
monchan1995 <simon.torii@icloud.com>
2024-11-22 20:39:01 +0900

    Update models.py
    date_joinedフィールドを削除しました。元々あったので。

--- accounts/models.py
+++ accounts/models.py
@@ -5,4 +5,3 @@ from django.db import models
 class CustomUser(AbstractUser):
     icon_image = models.ImageField(upload_to="user/", null=True, blank=False)
     introduction = models.TextField(blank=True)
-    date_joined = models.DateTimeField(auto_now_add=True)
