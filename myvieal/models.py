ffd89fac637b180472062106eb5d886412ea0684
monchan <simon.torii@icloud.com>
2024-11-24 23:02:16 +0900

    adjusted to ruff varidation
    objectという変数名を使用するとruffに怒られるようなので、instanceに変更した。他にもフォーマットを整えた。

--- myvieal/models.py
+++ myvieal/models.py
@@ -15,7 +15,6 @@ class Movie(models.Model):
     created_by = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
     thumbnail = models.ImageField(verbose_name="さむね", blank=True, null=True, upload_to="images/")
 
-
     def __str__(self):
         return f"{self.title} (by: {self.created_by.username})"
 
