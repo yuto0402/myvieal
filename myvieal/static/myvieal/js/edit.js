0e716c42cb78e3fd23cd39e9e7e586dd010c1790
segoshi <chokokomint@icloud.com>
2024-12-02 01:25:28 +0900

    バリデーションによるjsの修正

--- myvieal/static/myvieal/js/edit.js
+++ myvieal/static/myvieal/js/edit.js
@@ -1,16 +1,14 @@
-document.getElementById("image-input").addEventListener("change", function(event) {
+document.getElementById("image-input").addEventListener("change", function (event) {
   var file = event.target.files[0];
   var thumbnailPreview = document.querySelector(".thumbnail-preview");
-
-
 
   // FileReaderで動画のURLを作成
   var reader = new FileReader();
 
-  reader.onload = function(e) {
-    thumbnailPreview.src = reader.result;  // 動画のプレビューを表示
-    thumbnailPreview.style.display = "block";  // プレビューを表示
-  }
+  reader.onload = function () {
+    thumbnailPreview.src = reader.result; // 動画のプレビューを表示
+    thumbnailPreview.style.display = "block"; // プレビューを表示
+  };
 
   // 動画ファイルを読み込む
   reader.readAsDataURL(file);
