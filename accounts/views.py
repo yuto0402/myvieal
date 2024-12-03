d73faccde5a8902e15c3294ac27e8e692905500a
segoshi <chokokomint@icloud.com>
2024-12-03 17:22:26 +0900

    フォロー機能 jsじゃない

--- accounts/views.py
+++ accounts/views.py
@@ -1,35 +1,75 @@
 # Create your views here.
-from django.shortcuts import render
-from django.urls import reverse_lazy, reverse
-from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView, TemplateView
+
+from typing import Any
+
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import redirect
+from django.urls import reverse
+from django.views.generic import DetailView, TemplateView, UpdateView
+
+from .forms import ProfileEditForm
 from .models import CustomUser
-from .forms import BaseCustomForm, CustomSignupForm, ProfileEditForm
+
 
 class ProfileView(LoginRequiredMixin, DetailView):
     model = CustomUser
-    context_object_name = 'user'
-    template_name = 'accounts/profile.html'
+    context_object_name = "user"
+    template_name = "accounts/profile.html"
 
     def get_object(self, queryset=None):
-        user = super().get_object(queryset)
-        return user
+        # リターンの前の宣言がいらないとruff-checkに言われました
+        return super().get_object(queryset)
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        movies = self.object.movie_set.all().order_by('-created_at')
-        context['movies'] = movies
-        context['movie_count'] = self.object.movie_set.count()
-        context['follower_count'] = self.object.followed_by.count()
+        movies = self.object.movie_set.all().order_by("-created_at")
+        context["movies"] = movies
+        context["movie_count"] = self.object.movie_set.count()
+        context["follower_count"] = self.object.followed_by.count()
         return context
+
 
 class ProfileEditView(LoginRequiredMixin, UpdateView):
     model = CustomUser
-    template_name = 'accounts/edit.html'
+    template_name = "accounts/edit.html"
     form_class = ProfileEditForm
 
     def get_success_url(self):
-        return reverse('Profile', kwargs={'pk': self.object.pk})
+        return reverse("Profile", kwargs={"pk": self.object.pk})
+
 
 class UserSettingView(LoginRequiredMixin, TemplateView):
-    template_name = 'accounts/setting.html'
+    template_name = "accounts/setting.html"
+
+
+class ProfileOthersView(LoginRequiredMixin, DetailView):
+    model = CustomUser
+    context_object_name = "user"
+    template_name = "accounts/profile_others.html"
+
+    # 共通して使う変数を設定
+    def setup(self, request, *args, **kwargs):
+        super().setup(request, *args, **kwargs)
+        self.object = self.get_object()
+        self.is_following = self.object in request.user.following.all()
+
+    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        movies = self.object.movie_set.all().order_by("-created_at")
+
+        extra_context = {
+            "is_following": self.is_following,
+            "movies": movies,
+            "movie_count": movies.count(),
+            "follower_count": self.object.followed_by.count(),
+        }
+        context.update(extra_context)
+        return context
+
+    def post(self, request, *args, **kwargs):
+        if self.is_following:
+            request.user.following.remove(self.object)
+        else:
+            request.user.following.add(self.object)
+
+        return redirect(request.path)
