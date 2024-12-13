36b683554fd2325a35b75ae70ea773d147d408b6
yutakatanaka19 <youdajiatianzhong@gmail.com>
2024-12-14 01:45:10 +0900

    new_model

--- accounts/migrations/0002_customuser_terms_of_service_version.py
+++ accounts/migrations/0002_customuser_terms_of_service_version.py
@@ -4,15 +4,14 @@ from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
-        ('accounts', '0001_initial'),
+        ("accounts", "0001_initial"),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='customuser',
-            name='terms_of_service_version',
+            model_name="customuser",
+            name="terms_of_service_version",
             field=models.IntegerField(default=0),
         ),
     ]
