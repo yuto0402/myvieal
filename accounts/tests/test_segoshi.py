204525a585a2e71bdbe05c36f23d694216206d1a
segoshi <chokokomint@icloud.com>
2025-01-08 16:23:46 +0900

    テストの追加と発見した問題の修正

---
+++ accounts/tests/test_segoshi.py
@@ -0,0 +1,48 @@
+from django.test import TestCase
+from django.urls import reverse
+
+from accounts.models import CustomUser
+
+
+class FollowButtonTest(TestCase):
+    def setUp(self):
+        self.tester_user = CustomUser.objects.create_user("tester")
+        self.test_user_followed = CustomUser.objects.create_user("test_followed")
+
+    def test_request_by_get(self):
+        self.client.force_login(self.tester_user)
+        response = self.client.get(reverse("follow_button"))
+        self.assertEqual(response.status_code, 405)
+
+    def test_user_is_not_logined(self):
+        response = self.client.post(reverse("follow_button"))
+        self.assertRedirects(
+            response,
+            f"{reverse("account_login")}?next={reverse("follow_button")}",
+            status_code=302,
+            target_status_code=200,
+        )
+
+    def test_user_is_not_following_target_user(self):
+        self.client.force_login(self.tester_user)
+        response = self.client.post(reverse("follow_button"), {"target_user_pk": self.test_user_followed.pk})
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json()["method"], "follow")
+        self.assertTrue(self.tester_user.following.filter(pk=self.test_user_followed.pk).exists())
+
+    def test_user_is_following_target_user(self):
+        self.client.force_login(self.tester_user)
+        self.tester_user.following.add(self.test_user_followed)
+        response = self.client.post(reverse("follow_button"), {"target_user_pk": self.test_user_followed.pk})
+        self.assertEqual(response.json()["method"], "unfollow")
+        self.assertFalse(self.tester_user.following.filter(pk=self.test_user_followed.pk).exists())
+
+    def test_with_not_exist_user(self):
+        self.client.force_login(self.tester_user)
+        response = self.client.post(reverse("follow_button"), {"target_user_pk": 100})
+        self.assertEqual(response.status_code, 404)
+
+    def test_with_request_user_itself(self):
+        self.client.force_login(self.tester_user)
+        response = self.client.post(reverse("follow_button"), {"target_user_pk": self.tester_user.pk})
+        self.assertEqual(response.status_code, 400)
--- myvieal/tests/test_segoshi.py
+++ myvieal/tests/test_segoshi.py
@@ -5,6 +5,12 @@ from accounts.models import CustomUser
 from myvieal.models import Movie
 
 
+def tool_request_by_post(test_case, url_name):
+    """test_caseとurl_nameを受け取り、test_caseのclientを使ってurl_nameと対応するURLにpostリクエストを送り、405エラーになることを検証する"""
+    response = test_case.client.post(reverse(url_name))
+    test_case.assertEqual(response.status_code, 405)
+
+
 class LibraryTests(TestCase):
     def setUp(self):
         self.tester_user = CustomUser.objects.create_user("tester")
@@ -63,14 +69,14 @@ class FavoriteButtonTest(TestCase):
             target_status_code=200,
         )
 
-    def test_user_is_not_favorite_target_user(self):
+    def test_user_is_not_favorite_target_movie(self):
         self.client.force_login(self.tester_user)
         response = self.client.post(reverse("favorite_button"), {"target_movie_pk": self.test_movie.pk})
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.json()["method"], "favorite")
         self.assertTrue(self.tester_user.movie_like.filter(pk=self.test_movie.pk).exists())
 
-    def test_user_is_favorite_target_user(self):
+    def test_user_is_favorite_target_movie(self):
         self.client.force_login(self.tester_user)
         self.tester_user.movie_like.add(self.test_movie)
         response = self.client.post(reverse("favorite_button"), {"target_movie_pk": self.test_movie.pk})
@@ -81,3 +87,23 @@ class FavoriteButtonTest(TestCase):
         self.client.force_login(self.tester_user)
         response = self.client.post(reverse("favorite_button"), {"target_movie_pk": 100})
         self.assertEqual(response.status_code, 404)
+
+
+class PrivacyPolicyTest(TestCase):
+    def test_request_by_post(self):
+        return tool_request_by_post(self, "privacy_policy")
+
+    def test_response_status_code_is_200_and_use_expected_template(self):
+        response = self.client.get(reverse("privacy_policy"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "myvieal/privacy_policy.html")
+
+
+class TermOfUseTest(TestCase):
+    def test_request_by_post(self):
+        return tool_request_by_post(self, "term_of_use")
+
+    def test_response_status_code_is_200_and_use_expected_template(self):
+        response = self.client.get(reverse("term_of_use"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "myvieal/term_of_use.html")
